83132078952bba96e55963160ad74c2aa9c7df8a
Артём <temasvg@gmail.com>
2024-12-29 23:15:17 +1000

    ДЗ 2.2

---
+++ src/org/skypro/skyshop/product/SimpleProduct.java
@@ -0,0 +1,25 @@
+package org.skypro.skyshop.product;
+
+public class SimpleProduct extends Product {
+    int priceProduct;
+
+    public SimpleProduct(String nameProduct, int productPrice) {
+        super(nameProduct);
+        this.priceProduct = productPrice;
+    }
+
+    @Override
+    public double getPriceProduct() {
+        return priceProduct;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", nameProduct, getPriceProduct());
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return false;
+    }
+}
