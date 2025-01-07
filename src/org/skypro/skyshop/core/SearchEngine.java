f541e80427b14cb0ed52051a1de447b7ba81e577
Артём <temasvg@gmail.com>
2025-01-07 20:51:50 +1000

    ДЗ 2.3

--- src/org/skypro/skyshop/core/SearchEngine.java
+++ src/org/skypro/skyshop/core/SearchEngine.java
@@ -3,16 +3,16 @@ package org.skypro.skyshop.core;
 import org.skypro.skyshop.product.ISearchable;
 
 public class SearchEngine {
-    ISearchable Searchable[];
+    ISearchable searchable[];
 
     public SearchEngine(int RangeArr) {
-        Searchable = new ISearchable[RangeArr];
+        searchable = new ISearchable[RangeArr];
     }
 
     public ISearchable[] search(String search) {
         ISearchable[] result = new ISearchable[5];
         int i = 0;
-        for (ISearchable Searchable : this.Searchable) {
+        for (ISearchable Searchable : this.searchable) {
             if (Searchable.searchTerm().toLowerCase().contains(search.toLowerCase())) {
                 result[i] = Searchable;
                 i++;
@@ -25,11 +25,11 @@ public class SearchEngine {
     }
 
     public void add(ISearchable elem) {
-        ISearchable[] buff = new ISearchable[Searchable.length + 1];
-        for (int i = 0; i < Searchable.length; i++) {
-            buff[i] = Searchable[i];
+        ISearchable[] buff = new ISearchable[searchable.length + 1];
+        for (int i = 0; i < searchable.length; i++) {
+            buff[i] = searchable[i];
         }
         buff[buff.length - 1] = elem;
-        Searchable = buff;
+        searchable = buff;
     }
 }
