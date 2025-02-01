a9860d192f2ff44b75e50abc1d23d9fa8700504f
Артём <temasvg@gmail.com>
2025-02-01 19:35:57 +1000

    комит

--- src/org/skypro/skyshop/core/SearchEngine.java
+++ src/org/skypro/skyshop/core/SearchEngine.java
@@ -5,32 +5,81 @@ import org.skypro.skyshop.product.ISearchable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class SearchEngine {
-    private List<ISearchable> searchable;
+    private final List<SearchObject> searchable;
+    private final SearchHistory searchHistory;
+
+
 
     public SearchEngine() {
         this.searchable = new ArrayList<>();
+        this.searchHistory = new SearchHistory();
     }
 
     public List<ISearchable> search(String search) {
+        searchHistory.addQuery(search);
         List<ISearchable> result = new ArrayList<>();
-        for (ISearchable Searchable : searchable) {
-            if (Searchable.searchTerm().toLowerCase().contains(search.toLowerCase())) {
-                result.add(Searchable);
+
+        String[] searchTerms = search.toLowerCase().split(" ");
+
+        for (SearchObject Searchable : searchable) {
+            boolean coincidences = true;
+            for (String term : searchTerms) {
+                if (!Searchable.Searchable.searchTerm().toLowerCase().contains(term)) {
+                    coincidences = false;
+                    break;
                 }
             }
+            if (coincidences) {
+                result.add(Searchable.Searchable);
+            }
+        }
         return result;
     }
 
     public void add(ISearchable elem) {
-        searchable.add(elem);
+        SearchObject addObject = new SearchObject();
+        addObject.Searchable = elem;
+        searchable.add(addObject);
     }
 
-    public ISearchable findTheMostSuitable(String search) throws BestResultNotFound{
-        if (searchable == null) {
-            throw new IllegalArgumentException("Объект поиска нге может быть null");
+    private void setFindIndex(String search) {
+        for (SearchObject searchObject : searchable) {
+            searchObject.FindIndex = countOccurrences(searchObject.Searchable.searchTerm(), search);
         }
+    }
+
+    private int getMaxIndex() {
+        int maxCount = Integer.MIN_VALUE;
+        for (SearchObject searchObject : searchable) {
+            if (searchObject.FindIndex > maxCount) {
+                maxCount = searchObject.FindIndex;
+            }
+        }
+        return maxCount;
+    }
+
+    private int getMinIndex() {
+        int minCount = Integer.MAX_VALUE;
+        for (SearchObject searchObject : searchable) {
+            if (searchObject.FindIndex < minCount && searchObject.FindIndex != 0) {
+                minCount = searchObject.FindIndex;
+            }
+        }
+        return minCount;
+    }
+
+    private int getMidIndex() {
+        int sum = 0;
+        for (SearchObject searchObject : searchable) {
+            sum += searchObject.FindIndex;
+        }
+        return sum / searchable.size();
+    }
+
+    public ISearchable findTheMostSuitable(String search) throws BestResultNotFound {
         if (searchable.toString().isEmpty()) {
             throw new IllegalArgumentException("Объект поиска не может быть пустым");
         }
@@ -38,31 +87,67 @@ public class SearchEngine {
             throw new IllegalArgumentException("Строка для поиска не может быть null или пустой");
         }
 
-        ISearchable mostSuitable = null;
-        int maxCount = 0;
+        searchHistory.addQuery(search);
+        setFindIndex(search);
 
-        for (ISearchable Searchable : this.searchable) {
-            int count = countOccurrences(Searchable.searchTerm(), search);
-
-            if (count > maxCount) {
-                maxCount = count;
-                mostSuitable = Searchable;
-            }
-        }
-
-        if (mostSuitable == null) {
+        int maxIndex = getMaxIndex();
+        if (maxIndex == 0) {
             throw new BestResultNotFound(search);
         }
-        return mostSuitable;
+
+        Random random = new Random();
+        ISearchable result = null;
+
+        for (SearchObject searchObject : searchable) {
+            if (maxIndex == searchObject.FindIndex) {
+                if (result == null) {
+                    result = searchObject.Searchable;
+                }
+                if (random.nextInt(100) < 50) {
+                    result = searchObject.Searchable;
+                }
+            }
+        }
+        return result;
     }
 
-    private int countOccurrences(String searchTerm, String search) {
+    public List<ISearchable> getMaxMidMinIndex(String search) {
+        setFindIndex(search);
+        searchHistory.addQuery(search);
+        List<ISearchable> minMidMax = new ArrayList<>();
+
+        minMidMax.addAll(getElementsByIndex(getMaxIndex(), 3));
+        minMidMax.addAll(getElementsByIndex(getMidIndex(), 3));
+        minMidMax.addAll(getElementsByIndex(getMinIndex(), 3));
+
+        return minMidMax;
+    }
+
+    private List<ISearchable> getElementsByIndex(int targetIndex, int limit) {
+        List<ISearchable> result = new ArrayList<>();
+        int count = 0;
+        for (SearchObject searchObject : searchable) {
+            if (searchObject.FindIndex == targetIndex && count < limit) {
+                result.add(searchObject.Searchable);
+                count++;
+            }
+        }
+        return result;
+    }
+
+    private int countOccurrences(String main, String sub) {
         int count = 0;
         int index = 0;
-        while ((index = searchTerm.indexOf(search, index)) != -1) {
+        while ((index = main.toLowerCase().indexOf(sub, index)) != -1) {
             count++;
-            index += search.length();
+            index += sub.length();
         }
         return count;
     }
+
+    public void printSearchHistory() {
+        searchHistory.printHistory();
+    }
+
 }
+
